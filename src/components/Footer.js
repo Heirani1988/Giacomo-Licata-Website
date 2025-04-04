bb2081f59133308fdbbeda5cbf101b8952970c84
Heirani1988 <chris.heirani@gmail.com>
2025-04-04 14:08:36 +0100

    Update Footer.js
    Update to socials

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -106,8 +106,8 @@ const Footer = () => {
         </FooterSection>
         <FooterSection>
           <SocialIcons>
-            <a href="https://facebook.com"><img src={facebookIcon} alt="Facebook" /></a>
-            <a href="https://instagram.com"><img src={instagramIcon} alt="Instagram" /></a>
+            <a href="https://www.facebook.com/p/Giacomo-Licata-Barber-Shop-100042074387343/"><img src={facebookIcon} alt="Facebook" /></a>
+            <a href="https://www.instagram.com/giacomolicatasbarbershop/"><img src={instagramIcon} alt="Instagram" /></a>
             <a href="https://www.google.com/search?q=giacomo+licata&rlz=1C1ASUC_enGB652GB652&oq=giacomo+licata&gs_lcrp=EgZjaHJvbWUqBggAEEUYOzIGCAAQRRg7MgYIARBFGDwyBggCEEUYPNIBCDI5MTBqMGo5qAIAsAIB&sourceid=chrome&ie=UTF-8#lrd=0x487191dd79204213:0xc3b41a8c31805052,1,,,,"><img src={googleIcon} alt="Google" /></a>
           </SocialIcons>
         </FooterSection>
